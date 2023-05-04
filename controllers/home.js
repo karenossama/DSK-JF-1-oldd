3fc8085d3fe250dbd14b77fa9cfd951f3be49529
farid <faridramy2003@gmail.com>
2023-05-04 13:42:34 +0300

    added the courses and the inner-courses to the files system

--- controllers/home.js
+++ controllers/home.js
@@ -1,12 +1,17 @@
 const home = (req, res) => {
   res.render("home");
 };
-
 const about = (req, res) => {
   res.render("about");
+};
+const courses = (req, res) => {
+  res.render("courses");
+};
+const courseInner = (req, res) => {
+  res.render("course-inner");
 };
 const contact = (req, res) => {
   res.render("contact");
 };
 
-module.exports = { home, about, contact };
+module.exports = { home, about, courses, courseInner, contact };
--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { home, about ,contact } = require("../controllers/home");
+const {
+  home,
+  about,
+  courses,
+  courseInner,
+  contact,
+} = require("../controllers/home");
 
 router.get("/", home);
 router.get("/about", about);
+router.get("/courses", courses);
+router.get("/course-inner", courseInner);
 router.get("/contact", contact);
 
 module.exports = router;
