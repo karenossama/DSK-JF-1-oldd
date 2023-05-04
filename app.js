70f77fd357b32d639bcebf791ea2e1209244f640
farid <faridramy2003@gmail.com>
2023-05-04 18:00:10 +0300

    added .env package

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const app = express();
-const port = 3000;
+require("dotenv").config();
 
 app.set("view engine", "ejs");
 app.set(express.json());
@@ -9,26 +8,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.use("/", require("./routes/home"));
-app.use("/signup", require("./routes/signUp"));
 app.use("/admin", require("./routes/admin"));
 
 app.use((req, res, next) => {
   res.status(404).render("404");
 });
 
-app.listen(port);
+app.listen(process.env.PORT || 3001);
 
-// mongoose
-//   .connect(
-//     "mongodb+srv://farid:farid@learnjs.wuejl3c.mongodb.net/users?retryWrites=true&w=majority"
-//   )
-//   .then((result) => {
-//     app.listen(port, () => {
-//       console.log(
-//         `Example app listening on -> http://localhost:${port}  & connect to the DataBase`
-//       );
-//     });
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
