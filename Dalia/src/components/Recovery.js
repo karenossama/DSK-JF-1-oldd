5ac414859e8f0f7bb4f6cacf1291ce4cc6821cb4
Dalia Raafat <daliaraafat03@gmail.com>
2023-05-04 15:29:01 +0200

    Modified components

--- Dalia/src/components/Recovery.js
+++ Dalia/src/components/Recovery.js
@@ -1,39 +1,41 @@
-import React from 'react'
-import styles from '../styles/Username.css'
-
+import React from "react";
+import styles from "../styles/Username.module.css";
 
 export default function Recovery() {
   return (
     <div className="container mx-auto">
-      <div className='flex justify-center items-center h-screen'>
-        <div className='glass'>
+      <div className="flex justify-center items-center h-screen">
+        <div className={styles.glass}>
           <div className="title flex flex-col items-center">
-            <h4 className='text-5xl font-bold'>Recovery</h4>
-            <span className='pt-4 text-xl text-center text-gray-500'>Enter OTP to recover password</span>
+            <h4 className="text-5xl font-bold">Recovery</h4>
+            <span className="pt-4 text-xl text-center text-gray-500">
+              Enter OTP to recover password
+            </span>
           </div>
-          <form className='pt-10'>
-          
-            <div className="textbox flex flex-col items-center gap-5" >
+          <form className="pt-10">
+            <div className="textbox flex flex-col items-center gap-5">
               <div className="input text-center">
-              <span className='py-4 text-sm text-left text-gray-500'>
-                Enter 6 digit OTP to send to your email address
-              </span>
+                <span className="py-4 text-sm text-left text-gray-500">
+                  Enter 6 digit OTP to send to your email address
+                </span>
               </div>
-              <input className='textbox' type="text" placeholder='OTP' />
-              <button className='btn' type='submit'>Recover</button>
-              <span id='errors'></span>
+              <input className={styles.textbox} type="text" placeholder="OTP" />
+              <button className={styles.btn} type="submit">
+                Recover
+              </button>
+              <span className={styles.errors} id="errors"></span>{" "}
             </div>
             <div className="text-center py-4">
-              <span className='text-gray-500'>Can't get OTP? <button className='text-red-500'>Resend</button></span>
+              <span className="text-gray-500">
+                Can't get OTP? <button className="text-red-500">Resend</button>
+              </span>
             </div>
           </form>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-
-
-// why using <Link to=""> instead of <a href=""> ? 
+// why using <Link to=""> instead of <a href=""> ?
 // --> when <a> is clicked the browser refreshes everytime and this is not how the browser works; as we want it to redirect directly to the pther page, so we use <link>
--- Dalia/src/components/Username.js
+++ Dalia/src/components/Username.js
@@ -1,52 +1,66 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import avatar from '../assets/profile.png'
-import { useFormik } from 'formik' // this is a library to acces the data of user form and we're gonna validate it using this format
-import styles from '../styles/Username.css'
-import { usernameValidate } from '../helpers/validate'
+import React from "react";
+import { Link } from "react-router-dom";
+import avatar from "../assets/profile.png";
+import { useFormik } from "formik"; // this is a library to acces the data of user form and we're gonna validate it using this format
+import styles from "../styles/Username.module.css";
+import { usernameValidate } from "../helpers/validate";
 
 export default function Username() {
-  
   const formik = useFormik({
     initialValues: {
-      Username: ''
+      Username: "",
     },
-    validate:usernameValidate,
-    validateOnBlur: false, // to validate the user info only when we click on the submit button 
+    validate: usernameValidate,
+    validateOnBlur: false, // to validate the user info only when we click on the submit button
     validateOnChange: false,
-    onSubmit: async values => {
-      console.log(values)
-    }
-  })
+    onSubmit: async (values) => {
+      console.log(values);
+    },
+  });
 
   return (
     <div className="container mx-auto">
-      <div className='flex justify-center items-center h-screen'>
-        <div className='glass'>
+      <div className="flex justify-center items-center h-screen">
+        <div className={styles.glass}>
           <div className="title flex flex-col items-center">
-            <h4 className='text-5xl font-bold'>Hello Again!</h4>
-            <span className='py-4 text-xl w-2/3 text-center text-gray-500'>Explore more by Connecting with us!</span>
+            <h4 className="text-5xl font-bold">Hello Again!</h4>
+            <span className="py-4 text-xl w-2/3 text-center text-gray-500">
+              Explore More by connecting with us.
+            </span>
           </div>
-          <form className='py-1' onSubmit={formik.handleSubmit}>
-            <div className='profile flex justify-center py-4'>
-              <img className='profile_img' src={avatar} alt="avatar" />
+
+          <form className="py-1" onSubmit={formik.handleSubmit}>
+            <div className="profile flex justify-center py-4">
+              <img src={avatar} className={styles.profile_img} alt="avatar" />
             </div>
-            <div className="textbox flex flex-col items-center gap-6" >
-              <input {...formik.getFieldProps('username')} className='textbox' type="text" placeholder='Username' />
-              <button className='btn' type='submit'>Let's Go!</button>
-              <span id='errors'></span>
+
+            <div className="textbox flex flex-col items-center gap-6">
+              <input
+                {...formik.getFieldProps("username")}
+                className={styles.textbox}
+                type="text"
+                placeholder="Username"
+              />
+              <button className={styles.btn} type="submit">
+                Let's Go
+              </button>
+              <span className={styles.errors} id="errors"></span>
             </div>
+
             <div className="text-center py-4">
-              <span className='text-gray-500'>Not a Member? <Link className='text-red-500' to="/register">Register Now</Link></span>
+              <span className="text-gray-500">
+                Not a Member{" "}
+                <Link className="text-red-500" to="/register">
+                  Register Now
+                </Link>
+              </span>
             </div>
           </form>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-
-
-// why using <Link to=""> instead of <a href=""> ? 
+// why using <Link to=""> instead of <a href=""> ?
 // --> when <a> is clicked the browser refreshes everytime and this is not how the browser works; as we want it to redirect directly to the pther page, so we use <link>
