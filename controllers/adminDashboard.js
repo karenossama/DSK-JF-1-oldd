9f4f8926c62a66948ba4488e5e61c4f2a36597d1
farid <faridramy2003@gmail.com>
2023-05-07 00:10:31 +0300

    added the admin course to the file system

--- controllers/adminDashboard.js
+++ controllers/adminDashboard.js
@@ -8,7 +8,7 @@ const banStudent = (req, res) => {
   res.render("admin/Ban-Student");
 };
 const editStudent = (req, res) => {
-  console.log('xx')
+  console.log("xx");
   res.render("admin/Edit-Student");
 };
 const searchBan = (req, res) => {
@@ -18,6 +18,22 @@ const searchEdit = (req, res) => {
   res.render("admin/Search-Edit");
 };
 
+const addCourse = (req, res) => {
+  res.render("admin/Add-courses");
+};
+const banCourse = (req, res) => {
+  res.render("admin/Ban-course");
+};
+const editCourse = (req, res) => {
+  res.render("admin/Edit-course");
+};
+const searchBanCourse = (req, res) => {
+  res.render("admin/searchremovec");
+};
+const searchEditCourse = (req, res) => {
+  res.render("admin/searcheditc");
+};
+
 module.exports = {
   adminDashboard,
   addStudent,
@@ -25,4 +41,9 @@ module.exports = {
   editStudent,
   searchBan,
   searchEdit,
+  addCourse,
+  banCourse,
+  editCourse,
+  searchBanCourse,
+  searchEditCourse,
 };
--- routes/admin.js
+++ routes/admin.js
@@ -7,6 +7,11 @@ const {
   editStudent,
   searchBan,
   searchEdit,
+  addCourse,
+  banCourse,
+  editCourse,
+  searchBanCourse,
+  searchEditCourse
 } = require("../controllers/adminDashboard");
 
 router.get("/", adminDashboard);
@@ -15,5 +20,11 @@ router.get("/student/ban", banStudent);
 router.get("/student/edit", editStudent);
 router.get("/student/search-ban", searchBan);
 router.get("/student/search-edit", searchEdit);
+
+router.get("/Add-course", addCourse);
+router.get("/ban-courses", banCourse);
+router.get("/edite-courses", editCourse);
+router.get("/search-ban-courses", searchBanCourse);
+router.get("/search-edite-courses", searchEditCourse);
 
 module.exports = router;
