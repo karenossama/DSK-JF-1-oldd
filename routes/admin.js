100a337ecaece7bfd1a19602171a8263ee1d7f99
farid <faridramy2003@gmail.com>
2023-05-06 23:23:55 +0300

    added the admin dashboard to the system (koki's part)

--- routes/admin.js
+++ routes/admin.js
@@ -2,18 +2,18 @@ const express = require("express");
 const router = express.Router();
 const {
   adminDashboard,
-  addStudent,
-  banStudent,
-  editStudent,
-  searchBan,
-  searchEdit
+  // addStudent,
+  // banStudent,
+  // editStudent,
+  // searchBan,
+  // searchEdit
 } = require("../controllers/adminDashboard");
 
 router.get("/", adminDashboard);
-router.get("/student/add", addStudent);
-router.get("/student/ban", banStudent);
-router.get("/student/edit", editStudent);
-router.get("/student/search-ban", searchBan);
-router.get("/student/search-edit", searchEdit);
+// router.get("/student/add", addStudent);
+// router.get("/student/ban", banStudent);
+// router.get("/student/edit", editStudent);
+// router.get("/student/search-ban", searchBan);
+// router.get("/student/search-edit", searchEdit);
 
 module.exports = router;
